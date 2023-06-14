e819ff18335811018a1f083dbeb41ee68943ca91
Abdul Hamid <Abdulhamidk0172@gmail.com>
2023-06-15 01:52:35 +0530

    Home about 2 added

---
+++ index.js
@@ -0,0 +1,6 @@
+// $('.Home-about-2-item').click(function (e) { 
+//     $('.Home-about-2-item').css('background-color', '#f65243')
+//     $('.Home-about-2-item, .Home-about-2-item > h4, .Home-about-2-item > a').css('color', '#fff')
+
+//     // $('.Home-about-2-item > h4').css('color', '#fff')
+// });
